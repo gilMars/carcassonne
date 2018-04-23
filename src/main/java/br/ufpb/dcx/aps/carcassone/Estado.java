5e7a9d5c70b28a3da657b93eba1b5d56b0e0d97c
gilMars <gilmar.lilium@gmail.com>
2018-04-23 13:00:04 -0300

    Adicionado sistema de verificação de estados do turno e da partida

---
+++ src/main/java/br/ufpb/dcx/aps/carcassone/Estado.java
@@ -0,0 +1,22 @@
+package br.ufpb.dcx.aps.carcassone;
+
+public enum Estado {
+	
+	P_ANDAMENTO("Em_Andamento"), T_INICIO("Início_Turno"), P_FIM("Partida_Finalizada");
+	
+	final private String nomeEstado;
+
+	Estado(String estado) {
+		this.nomeEstado = estado;
+	}
+	
+	public String getEstado() {
+		return nomeEstado;
+	}
+	
+	@Override
+	public String toString() {
+		return nomeEstado;
+	}
+	
+}
