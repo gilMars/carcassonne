220a289c7fc83523899bffa78d354ca54148817d
gilMars <gilmar.lilium@gmail.com>
2018-04-29 11:53:28 -0300

    Correção da classe TileComVertice

--- src/main/java/br/ufpb/dcx/aps/carcassone/tabuleiro/TileComVertice.java
+++ src/main/java/br/ufpb/dcx/aps/carcassone/tabuleiro/TileComVertice.java
@@ -1,8 +1,6 @@
-package br.ufpb.dcx.aps.carcassone;
+package br.ufpb.dcx.aps.carcassone.tabuleiro;
 
-import br.ufpb.dcx.aps.carcassone.tabuleiro.Tile;
-import br.ufpb.dcx.aps.carcassone.tabuleiro.TipoLado;
-import br.ufpb.dcx.aps.carcassone.tabuleiro.TipoTile;
+import br.ufpb.dcx.aps.carcassone.Lado;
 
 public class TileComVertice extends Tile {
 	
@@ -34,6 +32,38 @@ public class TileComVertice extends Tile {
 		while (getOrientacao() != Lado.NORTE) {
 			girar();
 		}
+	}
+	
+	public TipoLado getLadoNoroeste() {
+		return ladoNoroeste;
+	}
+	
+	public void setLadoNoroeste(TipoLado Noroeste) {
+		this.ladoNoroeste = Noroeste;
+	}
+	
+	public TipoLado getLadoNordeste() {
+		return ladoNordeste;
+	}
+	
+	public void setLadoNordeste(TipoLado Nordeste) {
+		this.ladoNordeste = Nordeste;
+	}
+	
+	public TipoLado getLadoSudeste() {
+		return ladoSudeste;
+	}
+	
+	public void setLadoSudeste(TipoLado ladoSudeste) {
+		this.ladoSudeste = ladoSudeste;
+	}
+	
+	public TipoLado getLadoSudoeste() {
+		return ladoSudoeste;
+	}
+	
+	public void setLadoSudoeste(TipoLado ladoSudoeste) {
+		this.ladoSudoeste = ladoSudoeste;
 	}
 
 }
